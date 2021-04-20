15554f5f17a220285385c813d3d0e19851989a63
SovietSpy2 <sovietspy2@users.noreply.github.com>
2021-04-20 14:06:41 +0200

    S3 config refactor

--- src/main/java/com/example/springawscloudformationdemo/config/S3Config.java
+++ src/main/java/com/example/springawscloudformationdemo/config/S3Config.java
@@ -6,26 +6,12 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.sqs.AmazonSQSAsync;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory;
-import org.springframework.cloud.aws.messaging.listener.QueueMessageHandler;
-import org.springframework.cloud.aws.messaging.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.PayloadArgumentResolver;
-import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
-import java.util.Collections;
-
-@Component
+@Configuration
 public class S3Config {
-
-    private AmazonS3 amazonS3;
 
     @Value("${key}")
     private String awsAccessKey;
@@ -33,23 +19,19 @@ public class S3Config {
     @Value("${secret}")
     private String awsSecretKey;
 
-    @PostConstruct
-    private void init() {
+    @Bean
+    public AmazonS3 amazonS3() {
 
         AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(
                 new BasicAWSCredentials(awsAccessKey, awsSecretKey)
         );
 
-        amazonS3 = AmazonS3ClientBuilder
+        return  AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(awsCredentialsProvider)
                 .withRegion(Regions.EU_CENTRAL_1)
                 .build();
 
-    }
-
-    public AmazonS3 getAmazonS3() {
-        return amazonS3;
     }
 
 }
